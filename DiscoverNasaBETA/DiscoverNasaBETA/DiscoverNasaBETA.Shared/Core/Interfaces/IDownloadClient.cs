4b0c849d530e73d87bd735eca8f2c62475034e9a
Akash <akash.professionals@outlook.com>
2015-06-04 22:42:42 +0530

    Added download client and it's interface.
    added two functions to retrieve string from uri and string passed.

---
+++ DiscoverNasaBETA/DiscoverNasaBETA/DiscoverNasaBETA.Shared/Core/Interfaces/IDownloadClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverNasaBETA.Core.Interfaces
+{
+    interface IDownloadClient
+    {
+        Task<string> DownloadString( string uri );
+        Task<string> DownloadString( Uri uri ); 
+    }
+}
---
+++ DiscoverNasaBETA/DiscoverNasaBETA/DiscoverNasaBETA.Shared/Core/WebServices/DownloadClient.cs
@@ -0,0 +1,36 @@
+﻿using DiscoverNasaBETA.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverNasaBETA.Core.WebServices
+{
+    class DownloadClient : IDownloadClient
+    {
+        public async Task<string> DownloadString( Uri uri )
+        {
+            //throw new NotImplementedException();
+
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetStringAsync(uri);
+            }
+
+            return null;
+        }
+
+        public async Task<string> DownloadString( string uri )
+        {
+            //throw new NotImplementedException();
+
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetStringAsync(uri);
+            }
+
+            return null;
+        }
+    }
+}
