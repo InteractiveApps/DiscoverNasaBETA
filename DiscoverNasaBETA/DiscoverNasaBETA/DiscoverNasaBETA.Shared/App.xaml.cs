d3641d11d4e4ca18d85f57b762fd491f9566afe8
Akash <akash.professionals@outlook.com>
2015-06-08 01:08:44 +0530

    removed catching components and all of its classes.

--- DiscoverNasaBETA/DiscoverNasaBETA/DiscoverNasaBETA.Shared/App.xaml.cs
+++ DiscoverNasaBETA/DiscoverNasaBETA/DiscoverNasaBETA.Shared/App.xaml.cs
@@ -95,7 +95,7 @@ namespace DiscoverNasaBETA
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(TestPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
---
+++ DiscoverNasaBETA/DiscoverNasaBETA/DiscoverNasaBETA.Shared/Caching Test/VM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DiscoverNasaBETA.Caching_Test
+{
+    public class VM : INotifyPropertyChanged
+    {
+
+        public List<item> items
+        {
+            get { return items; }
+            set
+            {
+                items=value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged("PersonName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event 
+        protected void OnPropertyChanged( string name )
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler!=null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+    public class item
+    {
+        public int index { get; set; }
+        public int count { get; set; }
+    }
+
+}
